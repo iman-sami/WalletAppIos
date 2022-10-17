2bbd02138ecaa84b63a482b9b932f0a3d6be4c9c
iman-sami <imansamifaith@gmail.com>
2022-10-17 21:22:48 +0300

    Initial Commit

---
+++ WalletAppIos/WalletAppIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletAppIosApp.swift
+//  WalletAppIos
+//
+//  Created by work on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct WalletAppIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
